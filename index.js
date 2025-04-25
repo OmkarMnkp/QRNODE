ff7e505d8ac7953cc598dfbbe483a79e70aa7370
omkar mankape <omkarmankape4065@gmail.com>
2025-04-25 23:48:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const QrCode = require('qrcode');
+const readline = require('readline-sync');
+
+// get the user input
+
+const input = readline.question("Enter text or URL to generate Qr code");
+
+// generate QR codeand save asimage
+QrCode.toFile('qrcode.png',input,{
+    color:{
+        dark:'#000000',
+        light:'#ffffff',
+    },
+},
+function (err){
+    if(err){
+        console.log("Error gQR code:",err)
+    }
+    else{
+        console.log("QR codegenerated and saved as qrcode.png " )
+    }
+}
+)
